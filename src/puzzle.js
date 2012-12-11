42ee73b43a30c7a9671c55d83f92bbe2a01c47c0
Guido Lorenz <code@superquadratic.net>
2012-12-11 15:21:47 +0100

    Use JS directly to allow debugging

--- src/puzzle.js
+++ src/puzzle.js
@@ -1,41 +1,53 @@
-// Parameters
-var NUM_TILES = 4;
+(function() {
+  "use strict";
+
+  // Parameters
+  var NUM_TILES = 4;
+
+  // Functions
+  function createTiles(img, numTiles) {
+    var tileSize = img.size.divide(numTiles * 2).floor().multiply(2);
+    var tiles = new Group();
+
+    for (var y = 0; y < numTiles; y++) {
+      for (var x = 0; x < numTiles; x++) {
+        var tileStart = tileSize.multiply([x, y]);
+        var tileRect = new Rectangle(tileStart, tileSize);
+
+        var tile = new Raster(img.getSubImage(tileRect));
+        tile.position = tileStart.add(tileSize.divide(2));
+
+        tiles.addChild(tile);
+      };
+    };
+
+    return tiles;
+  }
 
 
-function createTiles(img, numTiles) {
-  var tileSize = (img.size / numTiles / 2).floor() * 2;
-  var tiles = [];
+  function initPuzzle() {
+    // Load Image
+    var img = new Raster("puzzleImage");
 
-  for (var y = 0; y < numTiles; y++) {
-    for (var x = 0; x < numTiles; x++) {
-      var tileStart = tileSize * [x, y];
-      var tileRect = new Rectangle(tileStart, tileSize);
+    // Resize Canvas
+    view.viewSize = img.size;
+    img.position = view.center;
 
-      var tile = new Raster(img.getSubImage(tileRect));
-      tile.position = tileStart + tileSize / 2;
+    // Create Image Tiles
+    createTiles(img, NUM_TILES);
 
-      tiles.push(tile);
+    // Remove Original Image
+    img.remove();
+  }
+
+
+  window.onload = function() {
+    paper.install(window);
+    paper.setup(document.getElementById("puzzleCanvas"));
+    initPuzzle();
+
+    paper.view.onFrame = function() {
+      paper.view.draw();
     };
   };
-
-  return tiles;
-}
-
-
-function main() {
-  // Load Image
-  var img = new Raster("puzzleImage");
-
-  // Resize Canvas
-  view.viewSize = img.size;
-  img.position = view.center;
-
-  // Create Image Tiles
-  createTiles(img, NUM_TILES);
-
-  // Remove Original Image
-  img.remove();
-}
-
-
-main();
+}());
