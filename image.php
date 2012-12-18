1e8eaf92c1e0f6ea128bb72f17ca941490d34ef3
Guido Lorenz <code@superquadratic.net>
2012-12-18 13:59:49 +0100

    Allow filtering images by century

--- image.php
+++ image.php
@@ -9,13 +9,20 @@ define('PASSWORD', 'myPassword');
 $pdo = new PDO('mysql:dbname=' . DATABASE, USERNAME, PASSWORD);
 $pdo->query('SET NAMES "utf8"');
 
+/* Prepare WHERE statement to filter results */
+$century = intval($_GET['century']);
+$where = '';
+if ($century > 0 && $century < 2000) {
+    $where = ' WHERE year >= ' . $century . ' AND year < ' . ($century + 10);
+}
+
 /* Get random image offset */
-$countStatement = $pdo->query('SELECT COUNT(*) AS imageCount FROM CCPlayImages');
+$countStatement = $pdo->query('SELECT COUNT(*) AS imageCount FROM CCPlayImages' . $where);
 $row = $countStatement->fetch(PDO::FETCH_ASSOC);
 $randomOffset = mt_rand(0, $row['imageCount'] - 1);
 
 /* Fetch random image */
-$imageStatement = $pdo->prepare('SELECT id, pageid, title, author, archiveid, url, mime, sha1 FROM CCPlayImages LIMIT ?, 1');
+$imageStatement = $pdo->prepare('SELECT * FROM CCPlayImages' . $where . ' LIMIT ?, 1');
 $imageStatement->bindValue(1, $randomOffset, PDO::PARAM_INT);
 $imageStatement->execute();
 $image = $imageStatement->fetch(PDO::FETCH_ASSOC);
