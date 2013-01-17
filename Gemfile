80f8bcecf770e7111ed7bfc18f826c196286b17d
Guido Lorenz <code@superquadratic.net>
2013-01-17 11:43:53 +0100

    Add Gemfile for Ruby dependencies

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source :rubygems
+
+gem "sass", ">=3.2"
+gem "compass", ">=0.12"
+gem "animation", ">=0.1.alpha.3"
--- grunt.js
+++ grunt.js
@@ -11,6 +11,7 @@ module.exports = function(grunt) {
         require: [
           "animation"
         ],
+        bundleExec: true,
         linecomments: false
       }
     },
